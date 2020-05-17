4e0dc344557366216bda0510fa09e7820f410735
gonzalezenios <gonzalezeniood@gmail.com>
2020-05-17 18:03:37 -0300

    agregando mongoose

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
+const mongoose = require('mongoose')
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/auth'
 const app = express()
+
+mongoose.Promise = global.Promise 
+mongoose.connect(MONGO_URL)
+mongoose.connect.on('error', (err) => {
+    throw err;
+    process.exit(1);
+})
 
 app.use(session({
    secret: 'ESTO ES SECRETO',
